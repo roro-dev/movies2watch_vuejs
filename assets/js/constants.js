3c3a4d56edbcf188a50d707033b9eb13c2a2932d
Stef Rod <stefanedr.dev@gmail.com>
2020-02-21 12:40:36 +0100

    config + deplacement repertoires

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -4,4 +4,4 @@ const API_MDB = 'https://api.themoviedb.org/3';
 const API_M2W = 'http://localhost:8000/api';
 const API_IMG = 'https://image.tmdb.org/t/p/w500/';
 
-const TOKEN_TMDB = '7afd664dc7f4ac326fb4da1f35ecbb8e';
+const TOKEN_TMDB = config.TOKEN_TMDB;
