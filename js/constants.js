5db4681c4bd76d7f8e351bf7eb15d28bf356024b
Stef Rod <stefanedr.dev@gmail.com>
2020-02-21 12:24:50 +0100

    ajout token constants

--- js/constants.js
+++ js/constants.js
@@ -3,3 +3,5 @@
 const API_MDB = 'https://api.themoviedb.org/3';
 const API_M2W = 'http://localhost:8000/api';
 const API_IMG = 'https://image.tmdb.org/t/p/w500/';
+
+const TOKEN_TMDB = '7afd664dc7f4ac326fb4da1f35ecbb8e';
